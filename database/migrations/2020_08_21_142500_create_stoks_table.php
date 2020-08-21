5edbbd2d07eb2ed39514f6bf968bf41893f920ca
Mastuhin <mastuhin33@gmai.com>
2020-08-21 21:58:03 +0700

    migrasi

---
+++ database/migrations/2020_08_21_142500_create_stoks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stok', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('product_id')->unique();
+            $table->integer('stok');
+            $table->foreign('product_id')
+                  ->references('id')
+                  ->on('products')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stoks');
+    }
+}
---
+++ database/migrations/2020_08_21_143258_create_transaksis_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('invoice');
+            $table->uuid('product_id');
+            $table->string('product_name');
+            $table->integer('qty');
+            $table->timestamps();
+            $table->foreign('product_id')
+                  ->references('id')
+                  ->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksis');
+    }
+}
